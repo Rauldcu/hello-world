80336ffa40ef4381b477dc5b9f71f9e58fd03d1b
del Cerro Urculo <r.del.cerro.urculo@accenture.com>
2022-03-31 10:22:46 +0200

    Lombok dependency

--- demo/src/main/java/com/example/demo/Employee.java
+++ demo/src/main/java/com/example/demo/Employee.java
@@ -6,64 +6,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
 class Employee {
 
-  private @Id @GeneratedValue Long id;
-  private String name;
-  private String role;
+	private @Id @GeneratedValue Long id;
+	private @NonNull String name;
+	private @NonNull String role;
 
-  Employee() {}
-
-  Employee(String name, String role) {
-
-    this.name = name;
-    this.role = role;
-  }
-
-  public Long getId() {
-    return this.id;
-  }
-
-  public String getName() {
-    return this.name;
-  }
-
-  public String getRole() {
-    return this.role;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public void setRole(String role) {
-    this.role = role;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-
-    if (this == o)
-      return true;
-    if (!(o instanceof Employee))
-      return false;
-    Employee employee = (Employee) o;
-    return Objects.equals(this.id, employee.id) && Objects.equals(this.name, employee.name)
-        && Objects.equals(this.role, employee.role);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(this.id, this.name, this.role);
-  }
-
-  @Override
-  public String toString() {
-    return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
-  }
 }
